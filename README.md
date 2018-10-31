3ec9dd5f49ea1808dd6e028a39f68723f48e511f
Suchit Jain <sj535@snu.edu.in>
2018-10-31 17:17:50 +0530

    initial

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Edit-Distance
-Comparing strings using edit distance
+Also, write (maximum two pages) as how to execute your code and
+short comings of your code and explanation of your results.
+
+# Edit Distance Calculator
+Edit distance is one of the popular ways to measure the dissimilarities between two strings based on counting the number of operations required to transfer one string into the other string.
+
+There are 3 basic string operations, namely -
+* Insertion - no. of steps involved = 1
+* Deletion - no. of steps involved = 1
+* Substitution - no. of steps involved = 2
+
+If the characters of the strings are same then we do not require any operation i.e no. of steps involved = 0
+The aim is to compute the minimum no. of steps required to convert one string into another
+## Running the code
+To test code run the following command in the MATLAB command terminal
+  ```MATLAB
+  editdistmain.m
+  ```
+Enter the first and second strings between which edit distance is to be calculated.
+
+
+## Back-Tracing
+Back-Tracing is used to formulate the sequence of operations to follow to successfully transfer one string to other string.
+
+## Output Format
+* The minimum edit distance(value at D(M,N)) is displayed.
+* The sequence of operations in an ordered form are displayed.
+
+## Limitations and assumptions
+There was an ambiguity over the order of precedence of insertion, deletion and substitution operation.
+For back-tracing we need to store the choice we made to reach the particular value(i.e we either did a substitution, an insertion or a deletion). If the minima value for all 3 comes out to be the same then substitution is given the highest order of precedence.
+If value of substitution equals another operation and that is the minima then also substitution is still given a higher order of precedence.
+
+This assumption might affect the back-tracing operation order in some cases(though I have not found a case till now) but will not affect the value of minimum edit distance value.
---
+++ editdistmain.m
@@ -0,0 +1,87 @@
+clear all;
+close all;
+%%
+prompt = 'Enter the first string: ';
+str1 = upper(input(prompt, 's'));
+
+prompt = 'Enter the second string: ';
+str2 = upper(input(prompt, 's'));
+
+% computing length of each string
+M = length(str1);
+N = length(str2);
+
+% initialing the edit distance matrix with indexes in the first row and column
+D = zeros(M + 1,N + 1);
+for i = 2:M + 1
+    D(i, 1) = i - 1;
+end
+for j = 2:N + 1
+    D(1, j) = j - 1;
+end
+
+% initilizing another matrix to keep a track of the decisions on minima taken for each entry
+I = zeros(M + 1, N + 1);
+
+% computing the edit distance matrix iteratively
+for i = 2:M + 1
+    for j = 2:N + 1
+        % deletion operation
+        deletion = D(i - 1, j) + 1;
+        
+        % insertion operation
+        insertion = D(i, j - 1) + 1;
+        
+        % substitution operation
+        if str1(i - 1) ~= str2(j - 1)
+            substitution = D(i - 1, j - 1) + 2;
+        else
+            substitution = D(i - 1, j - 1) + 0;
+        end
+        
+        % finding minima of deletion, insertion and substitution operation,
+        % while also storing our choice in another matrix I
+        [D(i, j), I(i, j)] = min([deletion insertion substitution]);
+        
+        % giving substitution the highest order of precedence in case we
+        % get equal minima for multiple operations
+        if deletion == insertion && insertion == substitution || insertion == substitution && insertion < deletion || deletion == substitution && deletion < substitution
+            I(i, j) = 3;
+        end
+    end
+end
+
+fprintf('Edit Distance = %d\n', D(M, N));
+
+% backtracing from the last stored entry in I matrix to back trace the
+% operations(deletion/insertion/substitution) we took to reach it
+while i >= 1 && j >= 1
+    % checking for substitution
+    if I(i, j) == 3
+        if i - 1 == 0 || j - 1 == 0
+            break;
+        end
+        if str1(i-1) == str2(j - 1)
+            fprintf('%s -> unchanged -> %s\n', str1(i-1), str2(j - 1));
+        else
+            fprintf('%s -> substitute -> %s\n', str1(i-1), str2(j - 1));
+        end
+        i = i - 1;
+        j = j - 1;
+    % checking for insertion
+    elseif I(i, j) == 2
+        if j - 1 == 0
+            break;
+        end
+        fprintf('* -> insertion -> %s\n', str2(j - 1));
+        j = j - 1;
+    % if not the above 2 then definitely deletion
+    else
+        if i - 1 == 0
+            break;
+        end
+        fprintf('%s -> deletion -> *\n', str1(i - 1));
+        i = i - 1;
+    end
+end
+
